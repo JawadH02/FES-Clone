2c9e49c06e58cb9118c9492094498c858a1916ea
JawadH02 <SelenaDevTeam@gmail.com>
2022-05-01 18:42:18 -0400

    added firebase auth page content

--- pages/firebase/auth.tsx
+++ pages/firebase/auth.tsx
@@ -1,12 +1,16 @@
 import { Nav, Sidebar, Display } from '../../components/index'
 
 const Auth = () => {
+  const title = 'Firebase Authentication'
+  const description = `In this video we will learn how to use Firebase Authentication with React to build advanced and secure applications that will impress recruiters.
+
+  Documentation: https://firebase.google.com/docs/auth/web/start`
   return (
     <div>
       <Nav />
       <div className="flex">
         <Sidebar />
-        <Display />
+        <Display title={title} description={description} />
       </div>
     </div>
   )
